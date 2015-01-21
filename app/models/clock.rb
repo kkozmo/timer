0a0206d917fdc4c822f3725d94182c70d1834afb
Kyle Kozma <kylekozma@gmail.com>
2015-01-21 09:00:36 -0800

    Adding methods to Clock model

--- app/models/clock.rb
+++ app/models/clock.rb
@@ -1,2 +1,14 @@
 class Clock < ActiveRecord::Base
+  def self.total_time
+    total = 0
+     all.each do |clock|
+      total += clock.time_end - clock.time_start
+     end
+     return (total / 3600).round(2)
+  end 
+
+  def self.reverse_time
+    sorted = all.order('created_at DESC')
+  end
 end
+
